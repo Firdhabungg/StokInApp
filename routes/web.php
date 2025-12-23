f550d20534603e0cb0a7fecbd3320f08b57c6616
Firdhabungg <firdhafirmansyah@dicky.gmail.com>
2025-12-23 15:35:57 +0700

    ubah routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
